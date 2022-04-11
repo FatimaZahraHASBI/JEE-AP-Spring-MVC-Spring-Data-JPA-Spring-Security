ff38502bc3c1951f053d8d40a0b1aab61b910396
Fatima Zahra Hasbi <hasbi.fatimazahra04@gmail.com>
2022-04-11 09:40:03 +0000

    initial commit

---
+++ src/main/java/com/example/tp8_etud_spring_mvc_jpa_security/Tp8EtudSpringMvcJpaSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.tp8_etud_spring_mvc_jpa_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp8EtudSpringMvcJpaSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tp8EtudSpringMvcJpaSecurityApplication.class, args);
+    }
+
+}
